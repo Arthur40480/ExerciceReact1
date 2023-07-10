1bddb49f2973d156949de7e0b6edf5d1739b4657
Arthur40480 <arthurgibert@free.fr>
2023-07-10 15:32:03 +0200

    Lessons useState

--- src/lessons/4.Le-state/Container.jsx
+++ src/lessons/4.Le-state/Container.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react"
+
 export default function Container() {
+  const countState = useState(0)
+  const [count, setState] = countState
+
+  function handleClick() {
+    if(count === 0) {
+      setState(99)
+    }else {
+      setState(0)
+    }
+  }
 
   return (
     <div>
+      <b>{countState[0]}</b>
+      <button onClick={() => handleClick()}>Incrémenter</button>
     </div>
   )
 }
