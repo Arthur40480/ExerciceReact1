12fdb25faa21982142aaad84a8ac5615f460c1f5
Arthur40480 <arthurgibert@free.fr>
2023-07-10 16:17:57 +0200

    Lesson Css

--- src/lessons/6.Utiliser-du-CSS/Container.jsx
+++ src/lessons/6.Utiliser-du-CSS/Container.jsx
@@ -1,8 +1,13 @@
+import "./Container.css"
+
 export default function Container() {
 
   return (
     <div>
-
+      <h1>Utiliser du CSS</h1>
+      <p className="txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Totam saepe culpa voluptates rerum, reprehenderit accusamus aspernatur. 
+      Non tempore, aspernatur iure aperiam iusto minus error perferendis magni, deleniti voluptate nam consequuntur!</p>
     </div>
   )
 }
