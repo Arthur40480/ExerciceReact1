c51c5781f8277e3f7d7af6483b25534bea3d5178
Arthur40480 <arthurgibert@free.fr>
2023-07-10 14:30:56 +0200

    First component

---
+++ src/lessons/2.Premier-composant/Listitem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Listitem() {
+  return (
+    <ul className="list">
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+    </ul>
+  )
+}
