9a8cd3c4e8a41c7ce1e01995a0886318a2af9183
Arthur40480 <arthurgibert@free.fr>
2023-07-11 10:59:16 +0200

    Lesson img

--- src/lessons/8.Utiliser-des-images/Container.jsx
+++ src/lessons/8.Utiliser-des-images/Container.jsx
@@ -1,8 +1,17 @@
+import fuji from "./assets/fuji.jpg"
+import triangle from "./assets/triangle.svg"
+
 export default function Container() {
+
 
   return (
     <div>
       <h1>Utiliser des images</h1>
+      <p>Image jpg /png/ webp/ etc</p>
+      <img src={fuji} alt="Mont fuji" />
+
+      <p>SVG</p>
+      <img src={triangle} alt="triangle" />
 
     </div>
   )
