276ab9f24c0d0c8c61f20bcbd49d368ae305fb92
Arthur40480 <arthurgibert@free.fr>
2023-07-12 10:12:56 +0200

    Lesson Controlled Uncontrolled Input

--- src/lessons/14.Controlled-uncontrolled-inputs/Container.jsx
+++ src/lessons/14.Controlled-uncontrolled-inputs/Container.jsx
@@ -1,10 +1,25 @@
+import { useState, useRef } from "react"
 import "./Container.css"
 
 export default function Container() {
 
+  const [state, setState] = useState("")
+
+  function handleInput(e) {
+    setState(e.target.value)
+  }
+
   return (
     <div>
-    
+      <label htmlFor="name">Votre nom:</label>
+      <input 
+      onChange={handleInput}
+      type="text" 
+      id="name" 
+      value={state}></input>
+      <button>Envoyer</button>
+
+      <p>Votre pseudo : {state ? state : "Rentrez votre pseudo"}</p>
     </div>
   )
 }
